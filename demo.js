1054726cfe5a1cc88738f6bc5a7129ffbc83306d
Andrey Lushnikov <lushnikov@chromium.org>
2020-06-16 09:28:07 -0700

    feat: first demo

---
+++ demo.js
@@ -0,0 +1,14 @@
+const {webkit, chromium, firefox} = require('playwright');
+
+(async () => {
+  for (const browserType of [webkit, firefox, chromium]) {
+    const browser = await browserType.launch();
+    const page = await browser.newPage();
+    await page.goto('https://github.com/microsoft/playwright');
+    await page.screenshot({
+      path: `screenshot-${browserType.name()}.png`,
+    });
+    await browser.close();
+    console.log('success: ' + browserType.name());
+  }
+})();
