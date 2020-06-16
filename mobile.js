05b99678713bdab39e802edb1e691f8736da4972
Andrey Lushnikov <lushnikov@chromium.org>
2020-06-16 09:46:32 -0700

    feat: more stuff!

--- mobile.js
+++ mobile.js
@@ -1,13 +1,27 @@
 const {webkit, chromium, firefox, devices} = require('playwright');
-const iPhone11Pro = devices['iPhone 11 Pro'];
 
 (async () => {
-  const browser = await webkit.launch({
+  const browser = await chromium.launch({
     headless: false,
+    proxy: {
+      server: 'http://proxy.aslushnikov.com:3128',
+      username: process.env.PROXY_USERNAME,
+      password: process.env.PROXY_PASSWORD,
+    },
   });
   const context = await browser.newContext({
-    ...iPhone11Pro,
+    geolocation: {
+      latitude: 51.508076,
+      longitude: -0.0993827,
+    },
+    permissions: ['geolocation'],
+    locale: 'de-DE',
+    colorScheme: 'dark',
   });
   const page = await context.newPage();
-  await page.goto('https://github.com/microsoft/playwright');
+  await page.goto('https://overreacted.io');
+  await page.waitForTimeout(1000);
+  await page.emulateMedia({
+    colorScheme: 'light',
+  });
 })();
